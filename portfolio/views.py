534ff7d55c8a5af63d76ebf8ac7e5182835303b7
Sebastián Felipe Pérez Leal <felipeperezle@gmail.com>
2022-12-23 18:18:50 -0500

    change name

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,4 +3,4 @@ from .models import Project
 
 def home(request):
     projects = Project.objects.all()
-    return render(request, 'index.html', {'projects': projects})
+    return render(request, 'home.html', {'projects': projects})
