e1a68c543694ac284a7d1e0d62585aa2a04e976b
Felipe Pérez Leal <seperezl@unal.edu.co>
2024-01-26 17:02:38 -0500

    Text about me added

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,27 +1,35 @@
 import React from 'react'
 import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { IoIosArrowDown } from 'react-icons/io'
 
 const Home = () => {
   return (
-    <div className='h-screen flex items-center justify-around'>
-      <div className='flex flex-col p-20 items-start justify-evenly h-full'>
+    <div className='h-screen flex flex-col items-center justify-center w-full'>
+      <div className='flex flex-col p-20 justify-around h-full w-full'>
         <div>
-          <p className='text-4xl text tracking-tight font-medium md:text-7xl'>{"Hello, I'm Felipe Pérez Leal."}</p>
-          <p className='text-1xl tracking-widest py-10 md:text-3xl'>SOFTWARE  DEVELOPER</p>
+          <p className='text-4xl text tracking-tight font-medium md:text-8xl'>{"Hello, I'm Felipe Pérez Leal."}</p>
+          <p className='text-1xl tracking-widest py-10 md:text-4xl'>SOFTWARE  DEVELOPER</p>
         </div>
         <div className='flex items-center'>
-          <button className='text-xs rounded-full border py-2 px-4 text-black border-black hover:bg-black hover:text-white transition-colors md:text-lg'>
+          <button className='text-xs rounded-full border py-2 px-4 text-stone-950 border-stone-950 hover:bg-stone-950 hover:text-white transition-colors md:text-lg'>
             <a href="/cv.pdf" target="_blank">
               Get my resume
             </a>
           </button>
-          <a href="https://www.linkedin.com/in/felipeperezleal/" target='_blank' className='px-11'>
-            <FaLinkedin size={20}/>
-          </a>
+          <div className='px-11'>
+            <a href="https://www.linkedin.com/in/felipeperezleal/" target='_blank'>
+              <FaLinkedin size={20} />
+            </a>
+          </div>
           <a href="https://www.github.com/felipeperezleal" target='_blank'>
             <FaGithub size={20} />
           </a>
         </div>
+      </div>
+      <div className='flex h-1/6 justify-center items-center'>
+        <a href="">
+          <IoIosArrowDown size={50} />
+        </a>
       </div>
     </div>
   )
