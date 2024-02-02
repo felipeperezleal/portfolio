58a703d4eb7f19a1151e2417cb3012986e34c9f2
Felipe Pérez Leal <seperezl@unal.edu.co>
2024-02-02 18:00:04 -0500

    Responsive contact fixes

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -18,19 +18,19 @@ const Contact = () => {
   return (
     <div className="h-screen bg-stone-950 px-20 py-6 md:px-64 md:py-20">
       <div className="flex flex-col justify-around h-full">
-        <p className="text-6xl tracking-tight font-medium text-white text-center md:text-8xl">
+        <p className="text-4xl tracking-tight font-medium text-white text-center md:text-8xl">
           {"Let's build something great together!"}
         </p>
         <div className="flex items-center justify-center space-x-2">
           <button
             onClick={copyToClipboard}
-            className="rounded-full border w-3/4 py-2 text-xl text-white hover:bg-stone-900 transition-colors md:w-1/2 md:py-3 md:text-2xl"
+            className="rounded-full border w-full py-2 text-sm text-white hover:bg-stone-900 transition-colors md:w-1/2 md:py-3 md:text-2xl"
           >
             seperezl@unal.edu.co
           </button>
           <a
             href="mailto: seperezl@unal.edu.co"
-            className="rounded-full p-1 ml-2 text-white hover:bg-stone-950 hover:text-stone-400 transition-colors"
+            className="rounded-full p-1 text-white hover:bg-stone-950 hover:text-stone-400 transition-colors"
             target="_blank"
           >
             <IoSend size={35} />
@@ -38,7 +38,7 @@ const Contact = () => {
         </div>
         <div className="flex flex-col items-center space-y-12">
           <p className="text-white text-2xl md:text-4xl">{"Connect with me:"}</p>
-          <div className="flex items-center justify-evenly space-x-32">
+          <div className="flex items-center justify-evenly space-x-10 md:space-x-32">
             <a
               href="https://www.linkedin.com/in/felipeperezleal/"
               target="_blank"
@@ -65,10 +65,10 @@ const Contact = () => {
         <div>
           <hr className="border-t border-white pb-2 md:pb-5" />
           <div className="flex flex-col items-center justify-center">
-            <p className="text-white">
+            <p className="text-white text-center">
               Website design and development by Felipe Pérez Leal
             </p>
-            <p className="text-white">All rights reserved © 2024</p>
+            <p className="text-white text-center">All rights reserved © 2024</p>
           </div>
         </div>
       </div>
