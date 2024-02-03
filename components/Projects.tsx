cd372c2fb87c4ddf20e5f966eb08053c20f95928
Felipe Pérez Leal <seperezl@unal.edu.co>
2024-02-02 19:12:22 -0500

    Projects sktech

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React from "react";
+import { FiArrowUpRight } from "react-icons/fi";
 
 const Projects = () => {
   return (
-    <div className='h-screen flex justify-center bg-stone-950 px-10 py-10 md:px-32 md:py-32'>
-      <div className='flex items-center w-full h-1/4 bg-white'>
-        
+    <div className="h-fit flex flex-col bg-stone-950 px-10 py-10 space-y-12 md:px-32 md:space-y-32">
+      <p className="text-white text-4xl font-medium tracking-tight md:text-8xl">
+        My work
+      </p>
+      <div className="flex flex-col items-center  md:flex-row">
+        <div className="bg-white w-full h-48 rounded-3xl md:w-2/5 md:h-72" />
+        <div className="flex flex-col items-center md:items-start md:pl-10">
+          <p className="text-white text-xl md:text-4xl">Tripster</p>
+          <p className="text-white text-xl md:text-4xl">Mobile developer</p>
+        </div>
       </div>
+      <div className="flex flex-col items-center md:flex-row">
+        <div className="bg-white w-full h-48 rounded-3xl md:w-2/5 md:h-72" />
+        <div className="flex flex-col items-center md:items-start md:pl-10">
+        <p className="text-white text-xl md:text-4xl">Tripster</p>
+        <p className="text-white text-xl md:text-4xl">Backend developer</p>
+        </div>
+      </div>
+      <div className="flex flex-col items-center md:flex-row">
+        <div className="bg-white w-full h-48 rounded-3xl md:w-2/5 md:h-72" />
+        <div className="flex flex-col items-center md:items-start md:pl-10">
+          <p className="text-white text-xl md:text-4xl">Comprara!</p>
+          <p className="text-white text-xl md:text-4xl">Full-Stack developer</p>
+        </div>
+      </div>
+      <a className="text-white items-center justify-center hover:bg-stone-950 hover:text-stone-300 transition-colors flex">
+        <p className="text-white text-xl md:text-4xl">See more on my GitHub</p>
+        <FiArrowUpRight size={24} />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
